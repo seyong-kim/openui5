fde597a02dd79a75af0efede1ead9da6e976ff60
seyong-kim <61368679+seyong-kim@users.noreply.github.com>
2020-09-09 17:36:53 +0900

    Add files via upload

---
+++ kit/webapp/controller/mainView.controller.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+  "com/test/openui5/controller/BaseController"
+], function(Controller) {
+  "use strict";
+
+  return Controller.extend("com.test.openui5.controller.mainView", {});
+});
